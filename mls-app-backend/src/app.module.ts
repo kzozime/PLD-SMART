d521b466e72e7277fb1ec4cbd1a8a04ccf1932f1
AymenOH <aymen.ouldhamouda.pro@gmail.com>
2021-04-26 12:02:16 +0200

    module register started with mongo

--- mls-app-backend/src/app.module.ts
+++ mls-app-backend/src/app.module.ts
@@ -3,8 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth/auth.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule,
+     MongooseModule.forRoot(
+       'mongodb://admin:PLD-SMART@mls-cluster.6ykob.mongodb.net/MLS-App?retryWrites=true&w=majority'
+       )],
   controllers: [AppController],
   providers: [AppService],
 })
--- mls-app-backend/src/auth/auth/auth.service.ts
+++ mls-app-backend/src/auth/auth/auth.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
+
 @Injectable()
 export class AuthService {
   userConnection(mail: string, passwd: string): string {
-    if (mail === 'aymen' && passwd === 'test') {
+    /*if (mail === 'aymen' && passwd === 'test') {
       return 'OK';
     } else {
       return 'NOT OK';
-    }
+    }*/
+
+    
+
+
   }
 }
