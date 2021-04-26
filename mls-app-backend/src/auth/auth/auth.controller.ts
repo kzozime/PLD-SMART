927c78d89424af46e8d91fa0c0f97bfac88c8f56
AymenOH <aymen.ouldhamouda.pro@gmail.com>
2021-04-26 11:09:57 +0200

    module Auth started

--- mls-app-backend/src/auth/auth/auth.controller.ts
+++ mls-app-backend/src/auth/auth/auth.controller.ts
@@ -1,9 +1,16 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService){
-        
-    }
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  connexion(
+    @Body('emailUser') emailUser: string,
+    @Body('passwordUser') passwordUser: string
+  ): string {
+    return this.authService.userConnection(emailUser, passwordUser);
+  }
 }
--- mls-app-backend/src/auth/auth/auth.service.ts
+++ mls-app-backend/src/auth/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  userConnection(mail: string, passwd: string): string {
+    if (mail === 'aymen' && passwd === 'test') {
+      return 'OK';
+    } else {
+      return 'NOT OK';
+    }
+  }
+}
