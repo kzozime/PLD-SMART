01a75ad29ec22a952111ff0d847ebc51d6e19cce
AymenOH <aymen.ouldhamouda.pro@gmail.com>
2021-04-27 10:40:53 +0200

    commit for riad

--- mls-app-backend/src/auth/auth/auth.controller.ts
+++ mls-app-backend/src/auth/auth/auth.controller.ts
@@ -6,7 +6,7 @@ import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
+  
   @Post()
   connexion(
     @Body('emailUser') emailUser: string,
