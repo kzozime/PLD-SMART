9d9e985e4c6add52db49f689fb94c75a26b12a1f
AymenOH <63114087+AymenOH@users.noreply.github.com>
2021-05-08 07:35:54 +0200

    Update register.controller.ts

--- mls-app-backend/src/register/register/register.controller.ts
+++ mls-app-backend/src/register/register/register.controller.ts
@@ -7,14 +7,16 @@ import { RegisterService } from './register.service';
 @Controller('register')
 export class RegisterController {
   constructor(private readonly registerService: RegisterService) {}
-
+  
   @Post()
   @Header('Access-Control-Allow-Origin', '*')
+  //Async function which returns ths created user in a http response
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.registerService.create(createUserDto);
   }
-
+  
   @Get()
+  //Async function which returns all users in database in a http response
   async findAll(): Promise<User[]> {
     return this.registerService.findAll();
   }
