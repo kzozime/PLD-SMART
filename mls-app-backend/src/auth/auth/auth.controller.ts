fae373402b8323915648c47904a275db80099460
AymenOH <63114087+AymenOH@users.noreply.github.com>
2021-05-08 07:48:38 +0200

    Update auth.controller.ts

--- mls-app-backend/src/auth/auth/auth.controller.ts
+++ mls-app-backend/src/auth/auth/auth.controller.ts
@@ -2,7 +2,6 @@
 import { BadRequestException, Body, Controller, Get, Header, Post, Req, Res, UnauthorizedException } from '@nestjs/common';
 import { User, UserDocument } from 'src/schemas/user.schema';
 import { AuthService } from './auth.service';
-import { JwtService } from '@nestjs/jwt';
 import { Response, Request } from 'express';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
@@ -11,12 +10,12 @@ import { InjectModel } from '@nestjs/mongoose';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService,
-              private jwtService: JwtService,
               @InjectModel(User.name) private readonly userModel: Model<UserDocument>) {}
 
   @Post()
   @Header('Access-Control-Allow-Origin', '*')
   @Header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+  //Async function which returns the connected user
   async login(
     @Body('emailUser') emailUser: string,
     @Body('passwordUser') passwordUser: string,
@@ -31,31 +30,6 @@ export class AuthController {
       throw new BadRequestException('invalide credentials');
     }
 
-    const jwt = await this.jwtService.signAsync({id: user.email});
-
-    response.cookie('jwt', jwt, {httpOnly : true});
-
     return user;
-  }
-
-  @Get()
-  @Header('Access-Control-Allow-Origin', '*')
-  async getUser(@Req() request: Request){
-
-    try {
-      const cookie = request.cookies['jwt'];
-      const cookieData = await this.jwtService.verifyAsync(cookie);
-
-      if(!cookieData){
-        throw new UnauthorizedException();
-      }
-      const user = await this.userModel.findOne({email: cookieData['id']});
-      return user;
-    } catch (error) {
-      throw new UnauthorizedException();
-      
-    }
-
-    
   }
 }
